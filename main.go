8fbd88d8e489adf5fdcc8a07c59d5ec98f19662f
Kristian <krisfollegg>
2023-02-20 09:36:52 +0100

    endret ode

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 // Definerer flag-variablene i hoved-"scope"
 var fahr float64
 var out string
-var funfacts string
 
 // Bruker init (som anbefalt i dokumentasjonen) for å sikre at flagvariablene
 // er initialisert.
@@ -26,7 +25,6 @@ func init() {
 	flag.Float64Var(&fahr, "F", 0.0, "temperatur i grader fahrenheit")
 	// Du må selv definere flag-variablene for "C" og "K"
 	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
-	flag.StringVar(&funfacts, "funfacts", "sun", "\"fun-facts\" om sun - Solen, luna - Månen og terra - Jorden")
 	// Du må selv definere flag-variabelen for -t flagget, som bestemmer
 	// hvilken temperaturskala skal brukes når funfacts skal vises
 
